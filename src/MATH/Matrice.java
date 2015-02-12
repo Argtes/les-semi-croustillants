669a2cb116028de85d207f2a15aa9dcc2d1a3483
dowan <thomas.gosselin@etu.univ-lille1.fr>
2015-02-12 13:57:12 +0100

    matrice + affichage matrice

--- src/MATH/Matrice.java
+++ src/MATH/Matrice.java
@@ -1,10 +1,26 @@
 package MATH;
 
+import java.util.Random;
+
 public class Matrice {
 	int[][] matrice = new int[3][3];
 	
 	public Matrice(){
-		
+		Random r = new Random();
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				matrice[i][j] = r.nextInt(10);
+			}
+		}
+	}
+	
+	public void afficher(){
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				System.out.print(matrice[i][j] + " ");
+			}
+			System.out.println();
+		}
 	}
 	
 }
