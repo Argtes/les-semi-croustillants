1661ea75eb212031cb93d56a12749184ff55b25d
dowan <thomas.gosselin@etu.univ-lille1.fr>
2015-02-12 14:11:35 +0100

    ajout point

--- src/MATH/Point.java
+++ src/MATH/Point.java
@@ -1,7 +1,7 @@
 package MATH;
 
 public class Point {
-	int x, y, z;
+	private int x, y, z;
 	
 	public Point(int x, int y, int z){
 		this.x = x;
@@ -11,6 +11,23 @@ public class Point {
 	
 	public void afficher(){
 		System.out.println("x : " + x + "y : " + y + " z :" + z);
+	}
+	
+	public int[] getCoord(){
+		int[] tab = {x, y, z};
+		return tab;
+	}
+	
+	public void setX(int x){
+		this.x = x;
+	}
+	
+	public void setY(int y){
+		this.y = y;
+	}
+	
+	public void setZ(int z){
+		this.z = z;
 	}
 	
 }
