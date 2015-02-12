14ace5cea33b2ae47fbf1cf11e71b8e6f8083f57
Matthias Chombart <m.chombart@hotmail.fr>
2015-02-12 15:23:32 +0100

    rectification des classe: GtsLoad/Segment/Face/Sommet

--- src/MATH/GtsLoad.java
+++ src/MATH/GtsLoad.java
@@ -3,17 +3,13 @@ package MATH;
 import java.util.ArrayList;
 
 public class GtsLoad {
-	
-	
-	public GtsLoad(){
-		
+	String fichier = "fichiertexte.txt";
+	ArrayList<Segment> segment = new ArrayList<Segment>();
+	ArrayList<Sommet> Sommet = new ArrayList<Sommet>();
+	ArrayList<Face> Face = new ArrayList<Face>();
+
+	public GtsLoad() {
+
 	}
-	
-	
 
-	
-	
 }
-
-
-
