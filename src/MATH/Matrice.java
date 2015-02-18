ff5e5a59f37a516d9fa39dfdbe421c6d054df746
Marc-olivier PRATS <marc-olivier.prats@etudiant.univ-lille1.fr>
2015-02-18 11:25:01 +0100

    Matrice multiplier a completer d'urgence, je galere

--- src/MATH/Matrice.java
+++ src/MATH/Matrice.java
@@ -3,18 +3,23 @@ package MATH;
 import java.util.Random;
 
 public class Matrice {
-	int[][] matrice = new int[3][3];
-	
-	public Matrice(){
+	float[][] matrice;
+	int x;
+	int y;
+
+	public Matrice(int x, int y) {
+		this.x = x;
+		this.y = y;
+		matrice = new float[y][x];
 		Random r = new Random();
-		for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < 3; j++) {
+		for (int i = 0; i < y; i++) {
+			for (int j = 0; j < x; j++) {
 				matrice[i][j] = r.nextInt(10);
 			}
 		}
 	}
-	
-	public void afficher(){
+
+	public void afficher() {
 		for (int i = 0; i < 3; i++) {
 			for (int j = 0; j < 3; j++) {
 				System.out.print(matrice[i][j] + " ");
@@ -22,8 +27,8 @@ public class Matrice {
 			System.out.println();
 		}
 	}
-	
-	public String toString(){
+
+	public String toString() {
 		String s = "";
 		for (int i = 0; i < 3; i++) {
 			for (int j = 0; j < 3; j++) {
@@ -33,5 +38,61 @@ public class Matrice {
 		}
 		return s;
 	}
+
+	public void set(int x, int y, float val) {
+		matrice[x][y] = val;
+	}
+
+	public float get(int x, int y) {
+		return matrice[x][y];
+	}
+
+	public int sizeV() {
+		return x;
+	}
+
+	public int sizeH() {
+		return y;
+	}
+
+	public float[][] multiplier(Matrice m1) {
+		int x1=m1.sizeV();
+		int x2=x;
+		int y1=m1.sizeH();
+		int y2=y;
+		Matrice res = null;
+		if(x2==y1){
+			res = new Matrice(y2,x1);
+			float tempo;
+			for (int i = 0; i <x1 ; i++) {
+				tempo = 0;
+				for (int j = 0; j < 3; j++) {
+					for (int cpt = 0; cpt < 3; cpt++) {
+						tempo = (m1.get(i, j) * matrice[j][i]);
+					}
+					res.set(i, j, tempo);
+				}
+			}
+		}
+		for(int i=0; i<y;i++){
+			for(int j=0; i<x; i++){
+				matrice[j][i]=res.get(j, i);
+			}
+		}
+		return matrice;
 	
+	}
+public static void main(String[] args) {
+	Matrice ok = new Matrice(3, 3);
+	System.out.println(ok.toString());
+	Matrice ok1 = new Matrice(3, 3);
+	System.out.println();
+	System.out.println(ok1.toString());
+	ok.multiplier(ok1);
+	System.out.println();
+	System.out.println();
+	System.out.println(ok);
+}
+
+
 }
